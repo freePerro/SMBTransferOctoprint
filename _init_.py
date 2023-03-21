5603a0222184876fe95de42218f236d01e4fe175
freePerro <dougblumer51@gmail.com>
2023-03-20 20:51:31 -0500

    A bunch of editing

--- _init_.py
+++ _init_.py
@@ -6,13 +6,13 @@ from smbprotocol import smb2
 from octoprint.filemanager.storage import LocalFileStorage
 from octoprint.plugin import FileManagerPlugin
 
-class SmbTransferPlugin(FileManagerPlugin):
+class SMBTransferOctoprintPlugin(FileManagerPlugin):
 
     def get_action_commands(self):
         return dict(transfer_smb=["python", os.path.join(os.path.dirname(os.path.realpath(__file__)), "transfer_smb.py")])
 
     def initialize(self):
-        self._logger.info("SMB Transfer Plugin initialized.")
+        self._logger.info("SMB Transfer Octoprint Plugin initialized.")
 
     def on_event(self, event, payload):
         pass
@@ -20,25 +20,8 @@ class SmbTransferPlugin(FileManagerPlugin):
     def on_shutdown(self):
         pass
 
-    def get_assets(self):
-        return {
-            "js": ["js/smbtransfer.js"],
-            "less": ["less/smbtransfer.less"],
-            "css": ["css/smbtransfer.min.css"],
-    }
-
-    def get_template_configs(self):
-        return [
-            {
-                "type": "sidebar",
-                "custom_bindings": True,
-                "template": "smb_transfer_sidebar.jinja2",
-            }
-        ]
-
-
-__plugin_name__ = "SMB Transfer"
+__plugin_name__ = "SMB Transfer Octoprint"
 __plugin_version__ = "0.1.0"
 __plugin_description__ = "A plugin for OctoPrint to enable transfer of files to an SMB share."
 __plugin_pythoncompat__ = ">=3.8,<4"
-__plugin_implementation__ = SmbTransferPlugin()
+__plugin_implementation__ = SMBTransferOctoprintPlugin()
--- static/js/smbtransfer.js
+++ static/js/smbtransfer.js
@@ -1,17 +1,17 @@
 $(function () {
-    function SmbTransferViewModel(parameters) {
+    function SMBTransferOctoprintViewModel(parameters) {
         var self = this;
 
         self.settingsViewModel = parameters[0];
 
         self.transferToSmb = function () {
             $.ajax({
-                url: API_BASEURL + "plugin/smb_transfer/transfer",
+                url: API_BASEURL + "plugin/smbtransfer_octoprint/transfer",
                 type: "POST",
                 dataType: "json",
                 success: function () {
                     new PNotify({
-                        title: "SMB Transfer",
+                        title: "SMB Transfer Octoprint",
                         text: "File transfer started.",
                         type: "success",
                         hide: true,
@@ -19,7 +19,7 @@ $(function () {
                 },
                 error: function () {
                     new PNotify({
-                        title: "SMB Transfer",
+                        title: "SMB Transfer Octoprint",
                         text: "File transfer failed.",
                         type: "error",
                         hide: true,
@@ -30,8 +30,8 @@ $(function () {
     }
 
     OCTOPRINT_VIEWMODELS.push({
-        construct: SmbTransferViewModel,
+        construct: SMBTransferOctoprintViewModel,
         dependencies: ["settingsViewModel"],
-        elements: ["#sidebar_plugin_smb_transfer"],
+        elements: ["#sidebar_plugin_smbtransfer_octoprint"],
     });
 });
